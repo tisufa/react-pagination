f795e83af59d11c2e8a3617037ab0419a9b373df
Titus Sutio Fanpula <tisufamoney@gmail.com>
2025-06-14 14:49:49 +0700

    2. Menyiapkan Workspace

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import "./App.css";
+function App() {
+  return (
+    <div className="w-screen h-screen py-10 justify-center bg-slate-100">
+      <div className="w-[512px] mx-auto">
+        <h1 className="font-semibold  text-2xl">React Pagination</h1>
+        <p className="mb-3">
+          Create reusable pagination component from scratch
+        </p>
+        <div className="bg-white shadow-md p-3 rounded-md">
+          <p>Hello World</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
