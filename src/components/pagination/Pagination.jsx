ed9a3a5dfdea5de226cf039f6fb5abefb2941500
Titus Sutio Fanpula <tisufamoney@gmail.com>
2025-06-14 15:32:43 +0700

    4. Membuat Formula Paginations

--- src/components/pagination/Pagination.jsx
+++ src/components/pagination/Pagination.jsx
@@ -1,4 +1,57 @@
+import { useEffect, useState } from "react";
+
 const Pagination = () => {
+  const [state, setState] = useState({
+    paginations: [],
+    page: 1,
+  });
+
+  useEffect(() => {
+    const page = state.page;
+    const perPage = 10;
+    const totalRecord = 65;
+    const size = 5;
+    const half = Math.floor(size / 2);
+
+    const totalPage = Math.ceil(totalRecord / perPage);
+    console.log(totalPage, "totalPage");
+
+    const createPagination = () => {
+      if (totalPage <= size + 1) {
+        return Array.from({ length: totalPage }, (_, index) => index + 1);
+      } else {
+        let start = Math.max(2, page - half);
+        let end = Math.min(totalPage - 1, page + half);
+
+        if (page <= half + 1) {
+          start = 2;
+          end = size;
+        } else if (page >= totalPage - half) {
+          start = totalPage - size + 1;
+          end = totalPage - 1;
+        }
+
+        const range = Array.from(
+          { length: end - start + 1 },
+          (_, index) => start + index
+        );
+
+        return [
+          1,
+          ...(start > 2 ? ["..."] : []),
+          ...range,
+          ...(end < totalPage - 1 ? ["..."] : []),
+          totalPage,
+        ];
+      }
+    };
+
+    setState((prev) => ({
+      ...prev,
+      paginations: createPagination(),
+    }));
+  }, [state.page]);
+
   return (
     <ul className="inline-flex gap-1">
       <li>
@@ -9,9 +62,9 @@ const Pagination = () => {
           <em className="fas fa-chevron-left text-sm"></em>
         </a>
       </li>
-      {Array.from({ length: 8 }, (_, index) => index + 1).map((item) => (
+      {state.paginations.map((item) => (
         <li key={item}>
-          {item === 2 || item == 7 ? (
+          {item === "..." ? (
             <a
               href="#"
               className={`rounded-full flex cursor-default items-center justify-center w-7 h-7 `}
@@ -22,8 +75,14 @@ const Pagination = () => {
             <a
               href="#"
               className={`rounded-full flex items-center justify-center w-7 h-7 hover:bg-lime-500 transition-all duration-300 bg-lime-500/0 ${
-                item === 1 ? "!bg-lime-500/100" : ""
+                item === state.page ? "!bg-lime-500/100" : ""
               }`}
+              onClick={() =>
+                setState((prev) => ({
+                  ...prev,
+                  page: item,
+                }))
+              }
             >
               {item}
             </a>
@@ -43,4 +102,3 @@ const Pagination = () => {
 };
 
 export { Pagination };
-
